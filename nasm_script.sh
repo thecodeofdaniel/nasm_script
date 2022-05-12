f5b2556bb7057a56f46f2e481d21c3f6d84e49bd
Daniel <danielsemailforcodingstuff@yahoo.com>
2022-05-12 05:36:07 -0700

    Changed and added some comments and cleaned up some code

--- nasm_script.sh
+++ nasm_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-library_location="$HOME/Desktop/csci150_AssemblyLanguage/z_library/library.asm"
+library_location="$HOME/PATH/TO/LIBRARY/FILE.ASM"
 
 # If set to true then give name of your library .asm file
 let_script_find_library=false
-lib_name="library.asm"
+lib_name="LIBRARY.ASM"
 # set to true if you want to save your inputs even after exiting script
 create_input_history=false
 # Set to false if you want to remove "Exit the script with: Ctrl + C" line
@@ -21,40 +21,36 @@ RED="\e[31m"
 GRN="\e[32m"
 YELW="\e[33m"
 BLUE="\e[34m"
+RML="\e[1A\e[K"
 
 function validate_each_character()
 {
-    # local variables are visible to the functions they call 
     local errors=0
     local sel_file_count=0
 
-    # checks for the first character
+    # checks the first character
     if [ "${char_input[0]}" != 'e' ] && [ "${char_input[0]}" != 'd' ]; then
-        printf "${RED}'e' or 'd' should be the first character${EC}\n"
-        ((errors++))
+        printf "${RED}'e' or 'd' should be the first character${EC}\n"; ((errors++))
     fi
 
-    # goes through the rest of the characters
-    for ((i = 1 ; i < $sz_of_input ; i++))
-    do
+    # checks the rest of the characters
+    for ((i = 1 ; i < $sz_of_input ; i++)); do
         # if character is an integer
         if [[ ${char_input[$i]} =~ ^[0-9]+$ ]]; then
 
             # check if integer selected is on the list
             if [ ${char_input[$i]} == 0 ] || [ ${char_input[$i]} -gt $num_asm_files ]; then
-                printf "${RED}${char_input[$i]} is not on the list${EC}\n"
-                ((errors++))
+                printf "${RED}${char_input[$i]} is not on the list${EC}\n"; ((errors++))
             else 
-                # otherwise increment the file count
                 ((sel_file_count++))
             fi
         # otherwise that character must only be 'l'
         elif [ "${char_input[$i]}" != 'l' ]; then
-            printf "${RED}'${char_input[$i]}' is not allowed at position $(($i+1))${EC}\n"
-            ((errors++))
+            printf "${RED}'${char_input[$i]}' is not allowed at position $(($i+1))${EC}\n"; ((errors++))
         fi
     done
 
+    # if user has not selected any files then add all .asm files to the command
     if [ $sel_file_count == 0 ]; then 
         for ((i = 0 ; i < $num_asm_files ; i++)); do 
             char_input[$sz_of_input+$i]=$(($i+1))
@@ -62,11 +58,7 @@ function validate_each_character()
     fi
 
     # if there are no errors continue to the next function
-    if [ $errors -gt 0 ]; then 
-        user_input
-    else
-        check_for_main
-    fi
+    if [ $errors != 0 ]; then user_input; else check_for_main; fi
 }
 
 function check_for_main()
@@ -74,35 +66,29 @@ function check_for_main()
     # This will keep track of the amount of main files in command
     local counter=0
 
-    for ((i = 1 ; i < ${#char_input[@]} ; i++)) 
-    do
+    for ((i = 1 ; i < ${#char_input[@]} ; i++)); do
         # if character is an integer
         if [[ ${char_input[$i]} =~ ^[0-9]+$ ]]; then
 
             int=${char_input[$i]}
-            
+
             # checks .asm file has "_start" text
             check_for_start=$(cat "${asm_file[$int-1]}.asm" | grep "_start" | wc -l)
 
             # increments counter if .asm file is a main file
-            if [ $check_for_start == 2 ]; then 
-                counter=$((counter+1)) 
-            fi      
+            if [ $check_for_start == 2 ]; then counter=$((counter+1)); fi      
         fi
     done
 
     # if there's only one main file in command then continue to the next function
     if [ $counter == 0 ]; then
-        printf "${RED}Select one main file${EC}\n"
-        user_input
+        printf "${RED}Select one main file${EC}\n"; user_input
     elif [ $counter -gt 1 ]; then
-        printf "${RED}You have selected $counter main files, only select one${EC}\n"
-        user_input
+        printf "${RED}You have selected $counter main files, only select one${EC}\n"; user_input
     else
-        # saving the command in case user wants to run the program again
-        history -s "${char_input[@]}"
-        HISTCONTROL=ignoredups:erasedups
-
+        # saving the command in history variable
+        history -s "${char_input[@]}"; HISTCONTROL=ignoredups:erasedups
+        # saving this command so user just enters "Enter" to execute previous command
         prev_char_input=("${char_input[@]}")
 
         evaluate_command "${char_input[@]}"
@@ -111,15 +97,13 @@ function check_for_main()
 function prev_cmnd()
 {
     if [ ${#prev_char_input[@]} -gt 0 ]; then
-        printf '\033[1A\033[K' # removing the empty "Enter: " line
         # displays the previous command
-        printf "Enter: ${BOLD}${DIM}${YELW}"; echo -n ${prev_char_input[@]}; printf "${EC}\n"
-        # sends the command the evaluate_command function skipping the 'validate_each_character'
+        printf "${RML}Enter: ${BOLD}${DIM}${YELW}"; echo -n ${prev_char_input[@]}; printf "${EC}\n"
+        # sends the command the evaluate_command function skipping the 'validate_each_character' & "check_for_main"
         evaluate_command "${prev_char_input[@]}"
     else
         # if the previous command is empty
-        printf "${RED}No previous command${EC}\n"
-        user_input
+        printf "${RED}No previous command${EC}\n"; user_input
     fi
 }
 function print_asm_files()
@@ -129,7 +113,7 @@ function print_asm_files()
 
     # If there are no .asm files in directory
     if [ $num_asm_files == 0 ]; then
-        printf "${BOLD}${RED}No .asm files here!${EC}"; sleep 1; clear; exit 0
+        printf "${BOLD}${RED}No .asm files here!${EC}\n"; exit 0
     fi
 
     # Displays user how to exit script
@@ -147,15 +131,15 @@ function print_asm_files()
 
 function user_input()
 {    
-    # grabbing each character of user input and putting them into array
+    # grabbing each character from user input and putting them into array
     echo; read -r -e -p $'Enter: \e[1m\e[33m' -a char_input; printf "${EC}"    
 
-    # getting the number of characters from string besides 'space'
     sz_of_input=${#char_input[@]}
 
-    # being able to use the previous command by leaving the command blank
+    # user is able to run the previous command by leaving the prompt blank
     if   [ $sz_of_input == 0 ]; then
         prev_cmnd
+    # single input commands that clear screen, clear history, and show history
     elif [ $sz_of_input == 1 ]; then 
         local bool=false
         if   [ "${char_input[0]}" == 'c'  ]; then clear; bool=true;
@@ -189,7 +173,7 @@ function create_linking_command()
         link_cmd+=" '$main_file'.out"
     fi 
 
-    # this checks if the obj file was created, if it was then o_file_created is set to 1
+    # this checks if the obj file was created, o_file_created is set to 1 if so
     if [ "$file_name" == "$library_location" ]; then
         o_file_created=$(ls "${library_location%/*}" | grep "\b${library_location##*/}.o\b" | wc -l)
     else 
@@ -218,7 +202,7 @@ function remove_obj_files()
             local obj_file=$(ls | grep "\b${asm_file[$int-1]}.o\b" | wc -l)
             if [ $obj_file == 1 ]; then rm "${asm_file[$int-1]}.o"; fi
         else 
-
+            # removes the object file from library file
             local obj_file=$(ls ${library_location%/*} | grep "\b${library_location##*/}.o\b" | wc -l)
             if [ $obj_file == 1 ]; then rm "$library_location.o"; fi
         fi
@@ -245,24 +229,22 @@ function find_library()
 
 function evaluate_command()
 {
+    # passing in the array from parameter
     char_input=("$@")
 
     # creating a string for the link command
     link_cmd="ld -m elf_i386 -o"
     link_cmd_other=""
+    # keeping track of the number of obj files created
     num_obj_files=0
 
-    # going through each chracter in order to create the linking command
+    # going through each character after the first in order to create the linking command
     for ((i = 1 ; i < ${#char_input[@]} ; i++)); do
 
-        local char=${char_input[$i]}
-
-        if [[ $char =~ ^[0-9]+$ ]]; then 
-            int=$char
+        if [[ ${char_input[$i]} =~ ^[0-9]+$ ]]; then 
+            int=${char_input[$i]}
             create_linking_command "${asm_file[$int-1]}"
-        fi
-        
-        if [ $char == 'l' ]; then
+        else 
             find_library 
             create_linking_command "$library_location"
         fi
@@ -272,26 +254,22 @@ function evaluate_command()
 
 function execute_debug()
 {
-    remove_previous_out_file
+    # compares the number of obj files created with the size of the command minus one
+    if [ $num_obj_files == $((${#char_input[@]}-1)) ]; then 
 
-    if [ $num_obj_files == $((${#char_input[@]}-1)) ]
-    then 
         # executes the linking command with the string created in "evaluate command" function
         eval "$link_cmd$link_cmd_other"
 
         # determines if .out file was created
         local out_file=$(ls | grep "\b$main_file.out\b" | wc -l)
 
-        if [ $out_file == 1 ]
-        then 
+        if [ $out_file == 1 ]; then 
+        
             # if user chose 'e' then execute the main .out file
-            if [ ${char_input[0]} == 'e' ]
-            then
-                eval "./'$main_file'.out"
+            if [ ${char_input[0]} == 'e' ]; then
 
-                if ! pgrep -x "./'$main_file'.out" > /dev/null; then
-                    printf "Exited ${GRN}$main_file.out${EC}\n"
-                fi 
+                eval "./'$main_file'.out"; printf "Exited ${GRN}$main_file.out${EC}\n"
+
             # otherwise debug the main .out file
             else
                 eval "gdb --quiet '$main_file'.out"
@@ -312,12 +290,9 @@ function exit_script()
     if [ $display_how_to_exit = true ]; then num=$((num+4)); else num=$((num+2)); fi
 
     # This will remove the text from output (the enter prompt and list of files) 
-    echo 
-    for i in $(seq 1 $num); do
-        printf "\033[1A\033[K"
-    done
+    echo; for i in $(seq 1 $num); do printf ${RML}; done
 
-    # this will write the user's input history
+    # this will write the user's input into history
     if [ $create_input_history = true ]; then history -w; fi
 
     exit 0
@@ -325,8 +300,7 @@ function exit_script()
 
 ### MAIN ###
 
-# trap keyword catches signals that happen during execution
-# Ctrl + C signals SIGINT 
+# "trap" catches signals and "Ctrl + C" signals SIGINT 
 trap exit_script SIGINT
 
 # this will read user's input history which enables user to use the up/down array keys
